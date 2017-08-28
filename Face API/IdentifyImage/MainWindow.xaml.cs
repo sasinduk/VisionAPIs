ec7016651a53cc6fce3f952e5b8328276f4372a2
Sasindu Kavinda <kaviliya123@gmail.com>
2017-08-28 18:44:46 +0530

    Face API
    Demo for Face API in Microsoft Cognitive Services

---
+++ Face API/IdentifyImage/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.ProjectOxford.Face;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IdentifyImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("<your face API key>", "https://westus.api.cognitive.microsoft.com/face/v1.0");
+        
+
+
+        private const string personGroupId = "family33";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TestMethodAsync();
+        }
+
+        public async void TestMethodAsync()
+        {
+            try
+            {
+                string testImageFile = @"D:\test.jpg";
+
+                using (Stream s = File.OpenRead(testImageFile))
+                {
+                    var faces = await faceServiceClient.DetectAsync(s);
+                    var faceIds = faces.Select(face => face.FaceId).ToArray();
+
+                    var results = await faceServiceClient.IdentifyAsync(personGroupId, faceIds);
+                    foreach (var identifyResult in results)
+                    {
+                        Console.WriteLine("Result of face: {0}", identifyResult.FaceId);
+                        if (identifyResult.Candidates.Length == 0)
+                        {
+                            Console.WriteLine("No one identified" );
+                        }
+                        else
+                        {
+                            // Get top 1 among all candidates returned
+                            var candidateId = identifyResult.Candidates[0].PersonId;
+                            var person = await faceServiceClient.GetPersonAsync(personGroupId, candidateId);
+                            Console.WriteLine("Identified as {0} & Confident Level {1}", person.Name, identifyResult.Candidates[0].Confidence.ToString());
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
---
+++ Face API/WPFCognetive/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFCognetive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TestMethodAsync();
+        }
+
+
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("<your face API key>");
+
+        private const string personGroupId = "family33";
+
+
+
+        public async void TestMethodAsync()
+        {
+
+
+            try
+            {
+
+                await faceServiceClient.CreatePersonGroupAsync(personGroupId, personGroupId);
+                // Define Anna
+                CreatePersonResult friend1 = await faceServiceClient.CreatePersonAsync(
+                    // Id of the person group that the person belonged to
+                    personGroupId,
+                    // Name of the person
+                    "Indika Dalugama"
+                );
+
+                // Directory contains image files of Anna
+                string friend1ImageDir = @"C:\Users\a-sakavi\Pictures";
+
+
+                foreach (string imagePath in Directory.GetFiles(friend1ImageDir, "*.jpg"))
+                {
+                    using (Stream s = File.OpenRead(imagePath))
+                    {
+                        // Detect faces in the image and add to Anna
+                        await faceServiceClient.AddPersonFaceAsync(
+                            personGroupId, friend1.PersonId, s);
+                    }
+                }
+
+                // Do the same for Bill and Clare
+                await faceServiceClient.TrainPersonGroupAsync(personGroupId);
+
+                TrainingStatus trainingStatus = null;
+                while (true)
+                {
+                    trainingStatus = await faceServiceClient.GetPersonGroupTrainingStatusAsync(personGroupId);
+
+                    if (!(trainingStatus.Status.Equals("running")))
+                    {
+                        break;
+                    }
+
+                    await Task.Delay(1000);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+
+        }
+    }
+}
